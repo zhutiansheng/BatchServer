f55fee36a489fe989fa68c21a432d5a79a96a7a5
zhutiansheng <44465877+zhutiansheng@users.noreply.github.com>
2019-12-30 09:17:54 +0800

    Update MyRandomForest.R

--- src/MyRandomForest.R
+++ src/MyRandomForest.R
@@ -1,10 +1,12 @@
 myRF<-function(d,ntree,nodesize){
-  RandomForest <- randomForest(label ~ . ,data=d,importance=T,ntree=ntree,nodesize=nodesize,na.action=na.exclude)
+  colnames(d)<-paste0("a",colnames(d))
+  RandomForest <- randomForest(alabel ~ . ,data=d,importance=T,ntree=ntree,nodesize=nodesize,na.action=na.exclude)
   
   imps  <- data.frame(importance(RandomForest));
   impScore <- imps$MeanDecreaseAccuracy
   #return(impScore)
   imps <- imps[order(impScore,decreasing=T),]
   orderedFeatures <- rownames(imps)
+  orderedFeatures<-gsub("^a","",orderedFeatures)
   return(list(features=orderedFeatures,mod=RandomForest))
 }
